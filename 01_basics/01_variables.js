cb148c9626dcb73536c104113cbe90788d7863dc
NMundle <156112290+NMundle@users.noreply.github.com>
2025-05-15 10:18:29 +0000

    variables and datatype commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 136314782;
+let accountEmail = "googlenutan@gmail.com";
+var accountPassword = "726386289368";
+accouncity = "jaipur";
+
+/* prefer not to use var
+ because of issue in block scope and functional scope*/
+
+console.table([accountId,accountEmail,accouncity,accountPassword]);
