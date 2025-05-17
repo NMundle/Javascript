af8938f18faeb871c2f9189bbd2e058e7555812d
NMundle <156112290+NMundle@users.noreply.github.com>
2025-05-17 13:17:36 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = 33;
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typesof valueInNumber);
+
+
+// "33"=> 33
+//"33abc"=>NaN
+// True =>1; false=>0
+
+let isLoggedIn =0
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 3;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+
