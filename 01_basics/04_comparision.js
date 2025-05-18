38175986c5cf179589f9222aa81533b5a8c48bf4
NMundle <156112290+NMundle@users.noreply.github.com>
2025-05-18 06:03:50 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log("2">1);
+// console.log("02">=1);
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// the equity check and comparision both work differently
+//comparision convert null to number, treating it as 0
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+console.log("2"===2);
+//In case of triple equity check it should required the same data type
+
+
