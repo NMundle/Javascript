348b6814cb7c0406df868b4f53422f91897b4729
NMundle <156112290+NMundle@users.noreply.github.com>
2025-05-18 05:23:10 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,6 +1,6 @@
 let score = 33;
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
 // console.log(typesof valueInNumber);
@@ -14,10 +14,34 @@ let isLoggedIn =0
 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 let someNumber = 3;
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+//operations//
+
+let str1 = "hello"
+let str2 = " nutan"
+let str3 = str1+str2
+
+console.log(str3);
+
+
+//tricky operations//
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+
+
+
 
