a2a01487947aa3043dbb18debc31d5799a76e5f0
NMundle <156112290+NMundle@users.noreply.github.com>
2025-06-04 11:54:14 +0000

    dates and time

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,17 @@
+let myDate = new Date() // Date is an Object
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date ("01-4-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeSpamp = Date.now()
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDate())
+
