491c2939a4dcd8dfc5ec703800e061ad5ac8b7a6
NMundle <156112290+NMundle@users.noreply.github.com>
2025-06-13 08:12:44 +0000

    commit change

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,63 @@
+
+
+if(true){
+    let a =  100
+const b = 200
+var c = 300
+// console.log("Inner",c);
+
+
+}
+// console.log("Inner",c);
+
+function one(){
+    const username = "Max"
+
+    function two(){
+        const website = "youtube"
+      console.log(website);
+      
+        
+    }
+    // console.log(username);
+    console.log(username);
+    two()
+    
+}
+
+// one()
+
+
+if (true){
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+//     console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+
+const addTwo = function(num){
+    return num + 2
+   
+    
+}
+console.log(addTwo(5));
+ 
+
+ 
